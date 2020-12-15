e305320ccb43700bdc6b4b7aa687b3a8048c6e14
JhanviM <jhanvi.it@gmail.com>
2020-12-15 16:46:44 +0530

    Inital

---
+++ SupportIODemo.xcframework/ios-arm64_i386_x86_64-simulator/SupportIO.framework/Headers/SupportIO.h
@@ -0,0 +1,18 @@
+//
+//  SupportIO.h
+//  SupportIO
+//
+//  Created by Avinash Sondhi on 30/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SupportIO.
+FOUNDATION_EXPORT double SupportIOVersionNumber;
+
+//! Project version string for SupportIO.
+FOUNDATION_EXPORT const unsigned char SupportIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SupportIO/PublicHeader.h>
+
+
